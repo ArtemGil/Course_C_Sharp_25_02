57b29bca638c7116a84beb4757b709bb6e510bf1
Artem <gil.artem@ya.ru>
2023-02-25 18:37:01 +0300

    add HW 1_2

---
+++ Lesson_1/HW/1_2_HW/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает
+//на вход три числа и выдаёт максимальное из этих
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b && a > c)
+{
+    Console.Write("Max = "+a);    
+}
+
+if (b > a && b > c)
+{
+    Console.Write("Max = "+b);    
+}
+
+if (c > a && c > b)
+{
+    Console.Write("Max = "+c);    
+}
