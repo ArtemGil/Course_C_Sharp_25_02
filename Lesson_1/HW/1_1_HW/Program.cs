2962962447ecdfb947df609d5cb3f092c63ff286
Artem <gil.artem@ya.ru>
2023-02-25 14:58:38 +0300

    add HW 1_1

--- Lesson_1/HW/1_1_HW/Program.cs
+++ Lesson_1/HW/1_1_HW/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a > b)
+{
+    Console.Write("Max = "+a+"; ");    
+    Console.Write("Min = "+b); 
+}
+else
+{
+    Console.Write("Max = "+b+"; ");    
+    Console.Write("Min = "+a); 
+}
+
