1986168ead11a6ab7648f60978b309d6ad7dcb60
Artem <gil.artem@ya.ru>
2023-02-26 12:26:35 +0300

    Add HW 2_2

---
+++ Lesson_2/HW/2_2_HW/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int sum = ((num / 100) * 10) + (num % 10);
+    return sum;
+}
+
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
