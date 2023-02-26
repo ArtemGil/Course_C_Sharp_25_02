9dbbeac2c393f033e0b2200e95a4d219b69d8f06
Artem <gil.artem@ya.ru>
2023-02-26 11:04:22 +0300

    Add task 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// 3. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+//    14 -> нет, 46 -> нет, 161 -> да
+
+void Find(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+        Console.WriteLine("Yes!");
+    else
+        Console.WriteLine("No.");
+}
+Find(int.Parse(Console.ReadLine()!));
