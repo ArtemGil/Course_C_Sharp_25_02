548796900a0b57f5f97a95805d909f95c9a0306f
Artem <gil.artem@ya.ru>
2023-03-05 21:45:30 +0300

    add all HW

--- Lesson_4/HW/4_0_HW/Program.cs
+++ Lesson_4/HW/4_0_HW/Program.cs
@@ -1,10 +1,14 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) 
 // и возводит число A в натуральну степень B.
 
-double Degree(int num, int stepen)
+int Degree(int num, int stepen)
 {
-    double result = Math.Pow(num, stepen);
-    return result;
+    int sum = 1;
+    for (int i = 0; i < stepen; i++)
+    {
+        sum = sum * num;
+    }
+    return sum;
 }
 
 int A = int.Parse(Console.ReadLine()!);
