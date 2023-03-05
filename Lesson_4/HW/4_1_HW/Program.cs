a168ecaae4919d48acf1c0211dacff42e1c4009f
Artem <gil.artem@ya.ru>
2023-03-05 13:54:30 +0300

    add HW 4_2

---
+++ Lesson_4/HW/4_1_HW/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int SumNum(int num)
+{
+    int length = 1;
+    int temporary = num;
+    while (temporary > 0)
+    {
+        temporary = temporary / 10;
+        length = length * 10;
+    }
+    length = length / 10;
+    int sum = 0;
+
+    while (length > 1)
+    {
+        sum = sum + (num/length);
+        num = num - ()
+        length--;
+
+    }
+    return length;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
