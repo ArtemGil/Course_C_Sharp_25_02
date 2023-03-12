d747a179855f27d5541b8a9f9efa5642fa77c6d6
Artem <gil.artem@ya.ru>
2023-03-12 11:31:45 +0300

    all + hw#1

--- Lesson_6/3_0/Program.cs
+++ Lesson_6/3_0/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+
+string convert(int num_1)
+{
+    string rezult = "";
+    while (num_1 > 0)
+    {
+        rezult = num_1 % 2 + rezult;
+        num_1 = num_1 / 2;
+    }
+    return rezult;
+}
+
+Console.WriteLine(convert(12));
+
+
--- Lesson_6/4_0/Program.cs
+++ Lesson_6/4_0/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+void Fibnch(int num)
+{
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write(a + " ");
+        (a, b) = (b, a + b);
+    }
+}
+
+int n = int.Parse(Console.ReadLine()!);
+Fibnch(n);
